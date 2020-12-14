f2fc0cd2966950bf1b2cc05b9b89b5e6541654d1
gaojia <sofia7452@163.com>
2020-12-14 16:28:51 +0800

    init

---
+++ precache-manifest.0209459a268a8b4e19adf24ee47dd271.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2154b29350dcb0c2ad46c38392fb637c",
+    "url": "/accounting-system/index.html"
+  },
+  {
+    "revision": "f9ac62096404eba1ea9d",
+    "url": "/accounting-system/static/css/main.c94e95db.chunk.css"
+  },
+  {
+    "revision": "67413af35e620cf7c6e2",
+    "url": "/accounting-system/static/js/2.b84f003f.chunk.js"
+  },
+  {
+    "revision": "768165ae6e4455b0f754a8444c7b06fa",
+    "url": "/accounting-system/static/js/2.b84f003f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9ac62096404eba1ea9d",
+    "url": "/accounting-system/static/js/main.fb9d45de.chunk.js"
+  },
+  {
+    "revision": "10674aaaf1e824750872",
+    "url": "/accounting-system/static/js/runtime-main.25ddc2cd.js"
+  }
+]);
